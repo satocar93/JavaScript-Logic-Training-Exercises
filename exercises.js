dc433fe3ac68feda10e821132e36b924c1d77cd1
satocar93 <77933950+satocar93@users.noreply.github.com>
2021-05-14 13:49:37 -0500

    Update exercises.js

--- exercises.js
+++ exercises.js
@@ -157,7 +157,12 @@ eliminarCaracteres('xyz1, xyz2, xyz3, xyz4 y xyz5', 'xyz')
 
 // 9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
 
-// PENDIENTE PENDIENTE PENDIENTE PENDIENTE PENDIENTE 
+// Después de definir el código de número aleatorio entre 0 y 100, agrupamos y sumamos 500
+// Math.round((Math.random() * 100) + 500)
+
+const aleatorio = () => console.info(Math.round((Math.random() * 100) + 500));
+
+aleatorio();
 
 // 10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
 
@@ -176,7 +181,12 @@ capicua(123321);
 // 11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
 // Uso de loop for y método .reduce()
 const factorial = (numero = undefined) => {
-    if (!numero) return console.warn('No introduciste un número.')
+    if (numero === undefined) return console.warn('No introduciste un número.')
+    if (typeof numero != 'number') return console.error(`El valor '${numero}' ingresado NO es un número`);
+    if (numero === 0) return console.error('El número no puede ser cero.');
+    if (Math.sign(numero) === -1) return console.error('El número no puede ser negativo.');
+
+    
     let arreglo = [];
     for (let i = numero; i >= 1; i--) {
         arreglo.push(i)
